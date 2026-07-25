746ff716d6e261fee5e683dd1ab4a25d9b9ab5f7
Xavier Nishikawa <xavier.nishikawa@gmail.com>
2026-07-25 16:13:07 -0400

    diag: build 10 vanilla mode, zero injection, bisect composer failure

--- Instano/Injector.swift
+++ Instano/Injector.swift
@@ -1,8 +1,13 @@
 import WebKit
 
 enum Injector {
+    // Diagnostic bisection: true = ship with NO injected script/CSS to test
+    // whether blocker.js itself breaks Instagram's composer.
+    static let vanillaDiagnosticMode = true
+
     static func makeContentController() -> WKUserContentController {
         let controller = WKUserContentController()
+        if vanillaDiagnosticMode { return controller }
         if let url = Bundle.main.url(forResource: "blocker", withExtension: "js"),
            let source = try? String(contentsOf: url, encoding: .utf8) {
             let script = WKUserScript(
